68aec5c112b2cf8f48f861ca920e755409c6611d
FND <fnd@fnd-osmosoft.(none)>
2009-03-25 00:22:36 +0800

    applied Python module template, made bake.py executable
    Signed-off-by: Phil Hawksworth <phawksworth@gmail.com>

--- bake.py
+++ bake.py
@@ -1,10 +1,25 @@
+#!/usr/bin/env python
+
+"""
+<module description>
+"""
+
+
 import sys, os, re
 
-# setup and read parameters
-recipe = sys.argv[1]
-filePaths = list()
-ingredients = dict()
-template = ''
+
+def main(argv=None):
+	# setup and read parameters
+	recipe = sys.argv[1]
+	filePaths = list()
+	ingredients = dict()
+	template = ''
+	# get started
+	print "Baking: " + recipe
+	parseRecipe(recipe)
+	collectIngredients()
+	populateTemplate()
+	return True
 
 
 def parseRecipe(recipe):
@@ -15,7 +30,7 @@ def parseRecipe(recipe):
 		line = line.rstrip("\n")
 		if line == "":
 			break
-		args = line.split(": ")	
+		args = line.split(": ")
 		if args[0] == "recipe":
 			# parse and ub recipes found.
 			parseRecipe(args[1])
@@ -39,7 +54,7 @@ def populateTemplate():
 	global filePaths, recipe
 	outputFile = recipe.rstrip(".recipe")
 	templateFile = open(template, 'r').read()
-	
+
 	# inspect template for sections to populate.
 	pattern = re.compile('(<!-- @@)(.*)(@@ -->)')
 	sections = pattern.finditer(templateFile)
@@ -52,8 +67,6 @@ def populateTemplate():
 	out.write(templateFile)
 
 
-# get started
-print "Baking: " + recipe
-parseRecipe(recipe)
-collectIngredients()
-populateTemplate()
+if __name__ == "__main__":
+	status = not main(sys.argv)
+	sys.exit(status)
